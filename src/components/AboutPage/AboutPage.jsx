0a0fdbc43940db284fe5897599fd7b64b2a6edab
Ben Hall <benjamhall@gmail.com>
2021-06-28 14:30:19 -0500

    created components

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,6 +9,7 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
+        
         <p>Have you ever wanted to know what are the top rated Hiking Spots in Minnesota?</p>
         <p>If so, you have come to the right place!</p>
         <p>We are so glad to have you join our Hiking Community of Adventurers who are dedicated to sharing the best hiking trails around to help you get out and enjoy spending time in nature.</p>
