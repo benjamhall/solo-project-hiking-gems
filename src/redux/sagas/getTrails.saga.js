f26822ef565c330c76ce65dcab052dfe789fe18f
Ben Hall <benjamhall@gmail.com>
2021-06-28 17:46:06 -0500

    setup trail router

--- src/redux/sagas/getTrails.saga.js
+++ src/redux/sagas/getTrails.saga.js
@@ -2,11 +2,18 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* getAllTrails () {
+    try {
+        const trails = yield axios.get('/api/movie');
+        console.log('get all:', movies.data);
+        yield put({ type: 'SET_TRAILS', payload: trails.data });
 
+    } catch {
+        console.log('get all error');
+    }
 }
 
-function* trailSaga() {
+function* getTrailsSaga() {
     yield takeLatest('FETCH_TRAILS', getAllTrails);
 }
 
-export default trailSaga;
+export default getTrailsSaga;
