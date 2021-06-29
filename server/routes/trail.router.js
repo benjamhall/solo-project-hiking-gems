3977b4b805b495401656a9677a9c252b95485a52
Ben Hall <benjamhall@gmail.com>
2021-06-29 14:45:46 -0500

    set up post in the trail router

--- server/routes/trail.router.js
+++ server/routes/trail.router.js
@@ -19,7 +19,26 @@ router.get('/', (req, res) => {
 
 // Sends a post request to the database and adds the new data
 router.post('/', (req, res) => {
-    // POST route code here
+    console.log(req.body);
+    
+    // Returning "id" will give us back the id of the created hike
+    const insertHikeQuery = `
+    INSERT INTO "hike" ("name", "location", "description")
+    VALUES ($1, $2, $3)
+    RETURNING "id";`
+
+    // This query makes the new hike entry
+    pool.query(insertHikeQuery, [req.body.name, req.body.location, req.body.description])
+    .then(result => {
+        console.log('New HIke Id:', result.rows[0].id) // We have the Id
+
+        //const createdHikeId = result.rows[0].id
+
+    // Catch for the query
+    }).catch(err => {
+        console.log(err);
+        res.sendStatus(500)
+    })
 });
 
 module.exports = router;
