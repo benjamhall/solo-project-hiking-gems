de910e315b999622e0b3336db47a74cf81a4f5dc
Ben Hall <benjamhall@gmail.com>
2021-06-29 13:58:05 -0500

    set up form for AddHike

--- src/components/AddHike/AddHike.jsx
+++ src/components/AddHike/AddHike.jsx
@@ -12,6 +12,23 @@ function AddHike() {
     return (
         <div>
             <h2>Add a Hike</h2>
+            <p>Please Fill Out This Form to Submit a New Hiking Trail:</p>
+            <form>
+                <TextField value={name}
+                onChange ={handleNameChange}
+                id="outlined-name" label="Hiking Trail Name" variant="outlined" />
+
+                <TextField value={location}
+                onChange={handleLocationChange}
+                id="outlined-location" label="Location" variant="outlined" />
+
+                <TextField value={description}
+                onChange={handleDescriptionChange}
+                id="outlined-basic" label="Description of Trail" variant="outlined" />
+
+            </form>
+            <Button onClick={handleCancel} type="submit" variant="contained" color="secondary"></Button>
+            <Button onClick={handleSave} type="submit" variant="contained" color="secondary"></Button>
         </div>
     );
 }
