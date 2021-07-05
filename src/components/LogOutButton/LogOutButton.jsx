fa21ac111d3ec705ef429d5d1dfea06e33fc5435
Ben Hall <benjamhall@gmail.com>
2021-07-05 19:35:45 -0400

    Add material UI to log out button

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
   return (
-    <button
+    <Button
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
+      onClick={() => dispatch({ type: 'LOGOUT' })} type="submit" variant="contained" color="secondary"
     >
       Log Out
-    </button>
+    </Button>
   );
 }
 
