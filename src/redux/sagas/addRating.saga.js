1506c391697a2eb0708b99865899b80057e7a185
Ben Hall <benjamhall@gmail.com>
2021-07-07 22:28:18 -0400

    set up add rating saga

--- src/redux/sagas/addRating.saga.js
+++ src/redux/sagas/addRating.saga.js
@@ -2,7 +2,18 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* addRating(action) {
-  
+    console.log('in add Rating saga', action.payload)
+    try {
+        // Axios request to add rating to the user's ratings
+        yield axios.post(`/api/rating`, { details: action.payload });
+        console.log('Rating', action.payload)
+
+        // Fetch all of the existing favorites to update DOM
+        yield put({ type: 'FETCH_FAVORITES' });
+
+    } catch (error) {
+        console.log('Error in add Rating Saga', error);
+    }
 
 }
 
