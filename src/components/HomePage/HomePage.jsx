07f6783ec8786b47ae8c37d8c228abf30be7d09a
Ben Hall <benjamhall@gmail.com>
2021-07-08 21:15:05 -0400

    update user page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,19 +1,50 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+// Material UI imports
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
-function HomePage(props) {
-    // Using hooks we're creating local state for a "heading" variable with
-    // a default value of 'Functional Component'
-    const store = useSelector((store) => store);
+function HomePage() {
+    // Material UI
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+        },
+        control: {
+            padding: theme.spacing(2),
+            margin: 'auto',
+        },
+    }));
+
+    const classes = useStyles();
+    // End Material UI
+
+    const history = useHistory();
     const [heading, setHeading] = useState('Functional Component');
 
     return (
-        <div>
-            <h2>{heading}</h2>
-        </div>
+        <Grid
+            container
+            className={classes.root}
+            spacing={2}
+            alignItems="center"
+            direction="column"
+        >
+            <Grid item xs={12}>
+                <h2>Welcome {heading}!</h2>
+                <p>We are so glad to have you join our Hiking Community of Adventurers dedicated to sharing Hidden Hiking Gems to hel you get out and enjoy spending more time in nature!</p>
+                <p>Click here to check out our List of Hidden Hiking Gems</p>
+                <Button
+                    className="btn"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => { history.push('/list') }}
+                >
+                    Let's Start Hiking!</Button>
+            </Grid>
+        </Grid>
     );
 }
 
