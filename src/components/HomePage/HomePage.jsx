25d398a1856cb59ab8b2c7bcd6e0e36446b2bfcb
Ben Hall <benjamhall@gmail.com>
2021-07-10 15:21:46 -0400

    center text in homePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -38,7 +38,7 @@ function HomePage() {
                 <h2>Welcome, {user.username}!</h2>
             </Grid>
             <Grid item xs={12}>
-                <p>We are so glad to have you join our Hiking Community of Adventurers dedicated to sharing Hidden Hiking Gems to hel you get out and enjoy spending more time in nature!</p>
+                <p>We are so glad to have you join our Hiking Community of Adventurers dedicated to sharing Hidden Hiking Gems to help you get out and enjoy spending more time in nature!</p>
             </Grid>
             <Grid item xs={12}>
                 <p>Click here to check out our List of Hidden Hiking Gems</p>
